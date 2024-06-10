f5ea011b7727058b9e200342102aa0af5b42f44d
ankurshashwat <1nkurshashwat@gmail.com>
2024-06-10 23:39:49 +0530

    Implement server actions for handling user-related operations, managing questions, answers, search functionality, user interactions, and tagging

---
+++ lib/actions/search.action.ts
@@ -0,0 +1,84 @@
+"use server";
+
+import Answer from "@/models/answer";
+import Question from "@/models/question";
+import Tag from "@/models/tag";
+import User from "@/models/user";
+import { SearchParams } from "@/types/shared";
+import { connectToDatabase } from "../mongoose";
+
+const SearchableTypes = ["question", "answer", "user", "tag"];
+
+export async function globalSearch(params: SearchParams) {
+  try {
+    await connectToDatabase();
+
+    const { query, type } = params;
+    const regexQuery = { $regex: query, $options: "i" };
+
+    let results = [];
+
+    const modelsAndTypes = [
+      { model: Question, searchField: "title", type: "question" },
+      { model: User, searchField: "name", type: "user" },
+      { model: Answer, searchField: "content", type: "answer" },
+      { model: Tag, searchField: "name", type: "tag" },
+    ];
+
+    const typeLower = type?.toLowerCase();
+
+    if (!typeLower || !SearchableTypes.includes(typeLower)) {
+      for (const { model, searchField, type } of modelsAndTypes) {
+        const queryResults = await model
+          .find({ [searchField]: regexQuery })
+          .limit(2);
+
+        results.push(
+          ...queryResults.map((item) => ({
+            title:
+              type === "answer"
+                ? `Answers containing ${query}`
+                : item[searchField],
+            type,
+            id:
+              type === "user"
+                ? item.clerkid
+                : type === "answer"
+                  ? item.question
+                  : item._id,
+          }))
+        );
+      }
+    } else {
+      const modelInfo = modelsAndTypes.find((item) => item.type === type);
+
+      console.log({ modelInfo, type });
+      if (!modelInfo) {
+        throw new Error("Invalid search type");
+      }
+
+      const queryResults = await modelInfo.model
+        .find({ [modelInfo.searchField]: regexQuery })
+        .limit(8);
+
+      results = queryResults.map((item) => ({
+        title:
+          type === "answer"
+            ? `Answers containing ${query}`
+            : item[modelInfo.searchField],
+        type,
+        id:
+          type === "user"
+            ? item.clerkId
+            : type === "answer"
+              ? item.question
+              : item._id,
+      }));
+    }
+
+    return JSON.stringify(results);
+  } catch (error) {
+    console.log(`Error fetching global results, ${error}`);
+    throw error;
+  }
+}
---
+++ lib/actions/tag.actions.ts
@@ -0,0 +1,139 @@
+"use server";
+
+import Question from "@/models/question";
+import Tag, { TagInterface } from "@/models/tag";
+import User from "@/models/user";
+import {
+    GetAllTagsParams,
+    GetQuestionsByTagIdParams,
+    GetTopInteractedTagsParams,
+} from "@/types/shared";
+import { FilterQuery } from "mongoose";
+import { connectToDatabase } from "../mongoose";
+
+export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
+  try {
+    connectToDatabase();
+
+    const { userId } = params;
+
+    const user = await User.findById(userId);
+
+    if (!user) throw new Error("User not found");
+
+    return [
+      { _id: "1", name: "tag" },
+      { _id: "2", name: "tag2" },
+    ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const { searchQuery, filter, page = 1, pageSize = 10 } = params;
+    const skipAmount = (page - 1) * pageSize;
+
+    const query: FilterQuery<typeof Tag> = {};
+
+    if (searchQuery) {
+      query.$or = [{ name: { $regex: new RegExp(searchQuery, "i") } }];
+    }
+
+    let sortOptions = {};
+
+    switch (filter) {
+      case "popular":
+        sortOptions = { questions: -1 };
+        break;
+      case "recent":
+        sortOptions = { createdAt: -1 };
+        break;
+      case "name":
+        sortOptions = { name: 1 };
+        break;
+      case "old":
+        sortOptions = { createdAt: 1 };
+        break;
+
+      default:
+        break;
+    }
+
+    const totalTags = await Tag.countDocuments(query);
+
+    const tags = await Tag.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    const isNext = totalTags > skipAmount + tags.length;
+
+    return { tags, isNext };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getQuestionsByTagId(params: GetQuestionsByTagIdParams) {
+  try {
+    connectToDatabase();
+
+    const { tagId, page = 1, pageSize = 10, searchQuery } = params;
+    const skipAmount = (page - 1) * pageSize;
+
+    const tagFilter: FilterQuery<TagInterface> = { _id: tagId };
+
+    const tag = await Tag.findOne(tagFilter).populate({
+      path: "questions",
+      model: Question,
+      match: searchQuery
+        ? { title: { $regex: searchQuery, $options: "i" } }
+        : {},
+      options: {
+        sort: { createdAt: -1 },
+        skip: skipAmount,
+        limit: pageSize + 1,
+      },
+      populate: [
+        { path: "tags", model: Tag, select: "_id name" },
+        { path: "author", model: User, select: "_id clerkId name picture" },
+      ],
+    });
+
+    if (!tag) {
+      throw new Error("Tag not found");
+    }
+
+    const isNext = tag.questions.length > pageSize;
+
+    const questions = tag.questions;
+
+    return { tagTitle: tag.name, questions, isNext };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getTopPopularTags() {
+  try {
+    connectToDatabase();
+
+    const popularTags = await Tag.aggregate([
+      { $project: { name: 1, numberOfQuestions: { $size: "$questions" } } },
+      { $sort: { numberOfQuestions: -1 } },
+      { $limit: 5 },
+    ]);
+
+    return popularTags;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
