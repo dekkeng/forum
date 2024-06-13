8f4b548b86ca6635ad935eb0908318d016820055
ankurshashwat <1nkurshashwat@gmail.com>
2024-06-13 08:55:34 +0530

    Implement server actions for handling user-related operations, managing questions, answers, search functionality, user interactions, and tagging

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,13 @@
-import { BADGE_CRITERIA } from "@/constants";
-import { BadgeCounts } from "@/types";
+import { BADGE_CRITERIA, CURRENCY_NOTATIONS } from "@/constants";
+import { JobPageFilters } from "@/constants/filters";
+import type {
+  BadgeCounts,
+  BadgeParams,
+  FilterProps,
+  RemoveUrlQueryParams,
+  UrlQueryParams,
+} from "@/types";
+import type { GetFormattedSalaryParams } from "@/types/shared";
 import { clsx, type ClassValue } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
@@ -9,67 +17,51 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const getTimestamp = (createdAt: Date): string => {
-  const now = new Date();
-  const timeDifference = now.getTime() - createdAt.getTime();
+  const now: Date = new Date();
+  const timeDifference: number = now.getTime() - createdAt.getTime();
 
-  const minute = 60 * 1000;
-  const hour = 60 * minute;
-  const day = 24 * hour;
-  const week = 7 * day;
-  const month = 30 * day;
-  const year = 365 * day;
+  const timeUnits: {
+    unit: string;
+    milliseconds: number;
+  }[] = [
+    { unit: "year", milliseconds: 365 * 24 * 60 * 60 * 1000 },
+    { unit: "month", milliseconds: 30 * 24 * 60 * 60 * 1000 },
+    { unit: "week", milliseconds: 7 * 24 * 60 * 60 * 1000 },
+    { unit: "day", milliseconds: 24 * 60 * 60 * 1000 },
+    { unit: "hour", milliseconds: 60 * 60 * 1000 },
+    { unit: "minute", milliseconds: 60 * 1000 },
+    { unit: "second", milliseconds: 1000 },
+  ];
 
-  if (timeDifference < minute) {
-    const seconds = Math.floor(timeDifference / 1000);
-    return `${seconds} ${seconds === 1 ? "second" : "seconds"} ago`;
-  } else if (timeDifference < hour) {
-    const minutes = Math.floor(timeDifference / minute);
-    return `${minutes} ${minutes === 1 ? "minute" : "minutes"} ago`;
-  } else if (timeDifference < day) {
-    const hours = Math.floor(timeDifference / hour);
-    return `${hours} ${hours === 1 ? "hour" : "hours"} ago`;
-  } else if (timeDifference < week) {
-    const days = Math.floor(timeDifference / day);
-    return `${days} ${days === 1 ? "day" : "days"} ago`;
-  } else if (timeDifference < month) {
-    const weeks = Math.floor(timeDifference / week);
-    return `${weeks} ${weeks === 1 ? "week" : "weeks"} ago`;
-  } else if (timeDifference < year) {
-    const months = Math.floor(timeDifference / month);
-    return `${months} ${months === 1 ? "month" : "months"} ago`;
-  } else {
-    const years = Math.floor(timeDifference / year);
-    return `${years} ${years === 1 ? "year" : "years"} ago`;
+  for (const { unit, milliseconds } of timeUnits) {
+    const time: number = Math.floor(timeDifference / milliseconds);
+    if (time >= 1) {
+      return `${time} ${unit}${time === 1 ? "s" : ""} ago`;
+    }
   }
+
+  return "Just now";
 };
 
-export const formatAndDivideNumber = (num: number): string => {
-  if (num >= 1000000) {
-    const formattedNum = (num / 1000000).toFixed(1);
-    return `${formattedNum}M`;
-  } else if (num >= 1000) {
-    const formattedNum = (num / 1000).toFixed(1);
-    return `${formattedNum}K`;
-  } else {
-    return num.toString();
-  }
+export const getFormattedNumber = (number: number): string => {
+  if (number < 1000) return number.toString();
+  if (number < 1000000) return `${(number / 1000).toFixed(1)}K`;
+  if (number < 1000000000) return `${(number / 1000000).toFixed(1)}M`;
+  return `${(number / 1000000000).toFixed(1)}B`;
 };
 
-export const getJoinedDate = (date: Date): string => {
-  const month = date.toLocaleString("default", { month: "long" });
-  const year = date.getFullYear();
-  const joinedDate = `${month} ${year}`;
+export const getFormattedJoinedDate = (date: Date): string => {
+  const month: string = date.toLocaleString("en", { month: "long" });
+  const year: number = date.getFullYear();
 
-  return joinedDate;
+  return `Joined ${month} ${year}`;
 };
 
-interface UrlQueryParams {
-  params: string;
-  key: string;
-  value: string | null;
-}
-
-export const formUrlQuery = ({ params, key, value }: UrlQueryParams) => {
+export const formUrlQuery = ({
+  params,
+  key,
+  value,
+}: UrlQueryParams): string => {
   const currentUrl = qs.parse(params);
 
   currentUrl[key] = value;
@@ -83,15 +75,10 @@ export const formUrlQuery = ({ params, key, value }: UrlQueryParams) => {
   );
 };
 
-interface RemoveUrlQueryParams {
-  params: string;
-  keysToRemove: string[];
-}
-
 export const removeKeysFromQuery = ({
   params,
   keysToRemove,
-}: RemoveUrlQueryParams) => {
+}: RemoveUrlQueryParams): string => {
   const currentUrl = qs.parse(params);
 
   keysToRemove.forEach((key) => {
@@ -107,14 +94,7 @@ export const removeKeysFromQuery = ({
   );
 };
 
-interface BadgeParam {
-  criteria: {
-    type: keyof typeof BADGE_CRITERIA;
-    count: number;
-  }[];
-}
-
-export const assignBadges = (params: BadgeParam) => {
+export const assignBadges = (params: BadgeParams): BadgeCounts => {
   const badgeCounts: BadgeCounts = {
     GOLD: 0,
     SILVER: 0,
@@ -136,3 +116,63 @@ export const assignBadges = (params: BadgeParam) => {
 
   return badgeCounts;
 };
+
+export const employmentTypeConverter = (type: string): string => {
+  let employmentType: string = "";
+
+  JobPageFilters.forEach((filter: FilterProps) => {
+    if (filter.value === type) {
+      employmentType = filter.name;
+    }
+  });
+
+  return employmentType;
+};
+
+export const getFormattedSalary = ({
+  min,
+  max,
+  currency,
+  period,
+}: GetFormattedSalaryParams) => {
+  if (!min || !max) return null;
+
+  const salaryInfo = {
+    symbol: CURRENCY_NOTATIONS[currency] || "$",
+    low: salaryFormatter(min, 1),
+    high: salaryFormatter(max, 1),
+    per: period ? `/${period.toLowerCase()}ly` : "",
+  };
+
+  const { symbol, low, high, per } = salaryInfo;
+
+  const formattedSalary = `${symbol}${low} - ${symbol}${high}${per}`;
+
+  return formattedSalary as string;
+};
+
+const salaryFormatter = (num: number, digits: number) => {
+  const lookup = [
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "k" },
+    { value: 1e6, symbol: "M" },
+    { value: 1e9, symbol: "G" },
+    { value: 1e12, symbol: "T" },
+    { value: 1e15, symbol: "P" },
+    { value: 1e18, symbol: "E" },
+  ];
+
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  const lookupItem = lookup
+    .slice()
+    .reverse()
+    .find((item) => num >= item.value);
+  return lookupItem
+    ? (num / lookupItem.value).toFixed(digits).replace(rx, "$1") +
+        lookupItem.symbol
+    : "0";
+};
+
+export function isValidImage(url: string) {
+  return /\.(jpg|jpeg|png|webp||svg)$/.test(url);
+}
